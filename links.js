85ac6ac5e6ba419f0d11ef469aaff53d44b9cb9b
Luciano Leite <157669068+lucianoleitedev@users.noreply.github.com>
2026-04-06 21:14:44 -0300

    Update links.js

--- links.js
+++ links.js
@@ -1,1 +1,8 @@
 
+const links = {
+  "1": "https://SEU_LINK_AFILIADO_1",
+  "2": "https://SEU_LINK_AFILIADO_2",
+  "3": "https://SEU_LINK_AFILIADO_3",
+  "4": "https://SEU_LINK_AFILIADO_4",
+  "5": "https://SEU_LINK_AFILIADO_5"
+};
